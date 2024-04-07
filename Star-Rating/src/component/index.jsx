34e4345c057473a66f0b9a34d69bed5b47c71690
PoojaMaddiwar <poojamaddiwar23@gmail.com>
2024-04-08 01:49:17 +0530

    creating components

---
+++ Star-Rating/src/component/index.jsx
@@ -0,0 +1,14 @@
+
+import "./style.css"
+
+const Comp1 =()=>{
+    return(
+        <>
+        <h3>
+            Star Rating project using React
+        </h3>
+        </>
+    )
+}
+
+export default Comp1;
